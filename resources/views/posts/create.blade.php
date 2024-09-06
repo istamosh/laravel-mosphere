091b07c759be5ee03521a901f49e579c6909703c
istamosh <63596969+istamosh@users.noreply.github.com>
2024-09-06 15:20:46 +0700

    displaying create form error on submit

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,12 +2,6 @@
     <x-header>Do you have something to share?</x-header>
 
     <div class="max-w-2xl mx-auto p-4 bg-slate-200 dark:bg-slate-900 rounded-lg">
-
-        {{-- displaying errors from PostController --}}
-        @foreach ($errors->all() as $error)
-            <p class="text-red-500">{{ $error }}</p>
-        @endforeach
-
         <form method="POST" action="/posts">
             {{-- using csrf directive for debug --}}
             @csrf
@@ -16,6 +10,10 @@
                     class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
                 <input type="text" id="default-input" name="title"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+
+                @error('title')
+                    <span class="text-red-500 text-sm">{{ $message }}</span>
+                @enderror
             </div>
 
             <div class="mb-6">
@@ -24,6 +22,10 @@
                 <textarea id="message" rows="4" name="content"
                     class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="Write your thoughts here..."></textarea>
+
+                @error('content')
+                    <span class="text-red-500 text-sm">{{ $message }}</span>
+                @enderror
             </div>
 
             <div class="mb-6">
